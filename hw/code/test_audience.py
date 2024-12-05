0aa926592f7ddb6c969b967cac2dcb185a76c736
Aleksandr Volokhov <volohovaleksandr1974@gmail.com>
2024-12-05 16:27:53 +0300

    тесты на создание и удаление источника

--- hw/code/test_audience.py
+++ hw/code/test_audience.py
@@ -25,4 +25,18 @@ class TestAudience(base.BaseCase):
         audience_page.delete_audience()
         audience_page.delete_audience_click()
 
+    def test_create_source(self, audience_page, login_data):
+        audience_page.create_audience()
+        audience_page.add_source()
+        audience_page.keywords()
+        audience_page.keywords_input("Тест")
+        audience_page.keywords_button_save()
+        audience_page.save_audience()
+        audience_page.selected_audiece()
+        audience_page.delete_audience()
+        audience_page.delete_audience_click()
+
+    
+
+
 
